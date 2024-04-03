466b64ae82a19fb6ace45d9eb6e1e7a3f9125574
miccn <73670306+miccn@users.noreply.github.com>
2024-04-03 13:43:00 -0400

    turned off autoplay and loop

--- js/video.js
+++ js/video.js
@@ -1,8 +1,11 @@
-var video;
+// link video class to video variable
+var video = document.querySelector(".video");
 
+// turn off video autoplay and looping
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video.autoplay = false;
+	video.loop = false;
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
