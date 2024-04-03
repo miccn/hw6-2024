32daa3255090daae4d7f8736cfc8d5533ac198bb
miccn <73670306+miccn@users.noreply.github.com>
2024-04-03 14:00:51 -0400

    implemeneted speed upa nd slow down:

--- js/video.js
+++ js/video.js
@@ -4,7 +4,9 @@ var video = document.querySelector(".video");
 // turn off video autoplay and looping
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video.pause();
 	video.autoplay = false;
+	video.autostart=false;
 	video.loop = false;
 });
 
@@ -16,8 +18,30 @@ document.querySelector("#play").addEventListener("click", function() {
 	document.querySelector("#volume").innerHTML = volume;
 });
 
+// pause the video onclick pause
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
 	video.pause();
 });
 
+// Slow the current video speed by 10% each time the button is clicked
+// and log the new speed to the console. 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = Math.max(0, video.playbackRate - 0.1);
+	console.log("playback rate: ", video.playbackRate);
+});
+
+// Increase the current video speed each time the button is clicked 
+// and log the new speed to the console.  Change it by an amount 
+// proportional to the slow down.
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = Math.min(1.0, video.playbackRate + 0.1);
+	console.log("playback rate: ", video.playbackRate);
+});
+
+// Advance the current video by 10 seconds. If the video 
+// length has been exceeded go back to the start of the video - no farther.
+// Log the current location of the video.
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("timestamp: "+  video.timestamp)
+});
